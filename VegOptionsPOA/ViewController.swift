b0cb2b164cec19cad3dc069c8c4abbcf4696996f
Camila <camila.prestes@yahoo.com.br>
2019-07-18 10:57:44 -0300

    Add search bar and table view

--- VegOptionsPOA/ViewController.swift
+++ VegOptionsPOA/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var testPlaces = ["One", "Two", "Three", "Four", "Five"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testPlaces.count
+    }
+    
 }
+
+
+
 
